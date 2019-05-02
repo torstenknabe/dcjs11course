31de350805858d7f140b45577a6032c5b8491903
Torsten <torsten@fireside21.com>
2019-05-02 18:21:38 -0400

    week 4

--- 05_functions/homework/starter.js
+++ 05_functions/homework/starter.js
@@ -4,8 +4,22 @@ Card Game of War Exercise Part 1.
 
 */
 
-let suits = ['hearts', 'clubs', 'spades', 'diamonds']
-let ranks = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king']
+let suits = ["hearts", "clubs", "spades", "diamonds"]
+let ranks = [
+  "ace",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+  "jack",
+  "queen",
+  "king"
+]
 
 // Given the above suits and ranks array, write a function called
 // createDeck that will take both as parameters and return a new array
@@ -19,19 +33,72 @@ let ranks = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'ni
 // Write a function called getRandomCard that will return one random
 // card from deckOfCards whenever it is called.
 
-// createDeck function
-
-function createDeck (suitsArr, ranksArr) {
-    let comboDeck = []
-    
+function createDeck(suits, ranks) {
+  let deck = []
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      let cardName = rank + " of " + suit
+      deck.push(cardName)
+    })
+  })
+  return deck
 }
 
-suits.forEach(function(suits[0], 1, suits) {
-    ranks.forEach(function(ranks[0], 1, ranks) {
+createDeck(suits, ranks)
 
-    });
-});
-
-// function getRandomCard () => {
-//     return deckOfCards[Math.ceil(Math.random() * deckOfCards.length)]
+// /** Solution Code */
+// // createDeck function
+// function createDeck(suitsArr, ranksArr) {
+//   // create a new array for combos
+//   const comboDeck = []
+//   // iterate over suits
+//   suitsArr.forEach(function(suit) {
+//     // iterate over ranks
+//     ranksArr.forEach(function(rank) {
+//       // create a new string that is a combination of current suit and rank
+//       const cardString = rank + " of " + suit
+//       // store new string in combos array
+//       comboDeck.push(cardString)
+//     })
+//   })
+//   // return new combos array
+//   return comboDeck
 // }
+
+// var deckOfCards = createDeck(suits, ranks)
+// console.log(deckOfCards)
+
+// /*
+
+//   Write a function called getRandomCard that will return one random
+//   card from deckOfCards whenever it is called.
+
+//   Don't worry about removing the card from deckOfCards.
+
+//   */
+
+// // create a function that returns one card
+// function getRandomCard(deckOfCards) {
+//   // randomly pick an index between (0,51)
+//   const rand = Math.floor(Math.random() * deckOfCards.length)
+//   // return string to user
+//   return deckOfCards[rand]
+// }
+// console.log(getRandomCard(deckOfCards))
+
+// // final product: one of the strings from deckOfCards
+
+// // function createDeck (suitsArr, ranksArr) {
+// //     let comboDeck = []
+
+// // }
+
+// // suits.forEach(function(suits[0], 1, suits) {
+// //     ranks.forEach(function(ranks[0], 1, ranks) {
+
+// //     });
+// // });
+
+// // function getRandomCard () => {
+// //     return deckOfCards[Math.ceil(Math.random() * deckOfCards.length)]
+// // }
--- 07-lab/starter/index.js
+++ 07-lab/starter/index.js
@@ -1,9 +1,9 @@
-const prompt = require('prompt-sync')({
+const prompt = require("prompt-sync")({
   autocomplete: []
 })
 
 // shuffle helper
-function shuffle (array) {
+function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1))
     let temp = array[i]
@@ -13,22 +13,8 @@ function shuffle (array) {
   return array
 }
 
-const suits = ['♥', '♣', '♠', '♦']
-const ranks = [
-  'A',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  'J',
-  'Q',
-  'K'
-]
+const suits = ["♥", "♣", "♠", "♦"]
+const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 const cardDict = {
   2: 2,
@@ -47,16 +33,16 @@ const cardDict = {
 }
 
 let player = {
-  name: 'Player',
+  name: "Player",
   hand: []
 }
 
 let computer = {
-  name: 'Computer',
+  name: "Computer",
   hand: []
 }
 
-function createDeck (ranks, suits) {
+function createDeck(ranks, suits) {
   var deck = []
 
   // In order to build a deck, we need 52 cards. Each card consists of a suit, a rank, and a numeric value.
@@ -65,8 +51,8 @@ function createDeck (ranks, suits) {
   //
   // We need to loop through each rank, and for each rank we need to loop through each suit. This way, we can
   // ensure that all 52 cards are created. This is called a nested loop.
-  ranks.forEach(function (rank) {
-    suits.forEach(function (suit) {
+  ranks.forEach(function(rank) {
+    suits.forEach(function(suit) {
       deck.push({
         name: `${rank}${suit}`,
         value: cardDict[rank]
@@ -78,3 +64,74 @@ function createDeck (ranks, suits) {
   // the deck is shuffled before we return it, so we use shuffle from lodash. https://lodash.com/docs/4.17.5#shuffle
   return shuffle(deck)
 }
+
+function playWar(player, computer) {
+  let round = 1
+  let earlyExit = false
+  const deck = createDeck(ranks, suits)
+
+  player.hand = deck.splice(deck.length / 2)
+  computer.hand = deck
+  console.log(player)
+  console.log(computer)
+
+  while (!earlyExit && player.hand.length > 0 && computer.hand.length > 0) {
+    console.log(`This is round ${round}`)
+
+    playRound(player, computer)
+
+    earlyExit = prompt("Next round? (y/n)", "y") !== "y"
+    round++
+  }
+}
+
+function playRound(player, computer) {
+  console.log("This is a round")
+
+  let playerWarCards = [player.hand.shift()]
+  let computerWarCards = [computer.hand.shift()]
+
+  console.log(`${player.name} has a ${playerWarCards[0].name}`)
+  console.log(`${player.name} has a ${computerWarCards[0].name}`)
+
+  let playerCardValue = playerWarCards[0].value
+  let computerCardValue = computerWarCards[0].value
+
+  if (playerCardValue > computerCardValue) {
+    // player wins
+    player.hand.push(...playerWarCards, ...computerWarCards)
+    console.log(`${player.name} wins!`)
+  } else if (playerCardValue < computerCardValue) {
+    // computer wins
+    computer.hand.push(...playerWarCards, ...computerWarCards)
+    console.log(`${computer.name} wins`)
+  } else {
+    // declare war
+    declareWar()
+  }
+  function declareWar() {
+    const playerDrawSize = player.hand.length >= 4 ? 4 : player.hand.length
+    const computerDrawSize =
+      computer.hand.length >= 4 ? 4 : computer.hand.length
+
+    const playerGainedCards = player.hand.splice(0, playerDrawSize)
+    const computerGainedCards = computer.hand.splice(0, computerDrawSize)
+
+    playerWarCards.unshift(...playerGainedCards)
+    computerWarCards.unshift(...computerGainedCards)
+
+    const prize = shuffle([...playerWarCards, ...computerWarCards])
+
+    if (playerWarCards[0].value > computerWarCards[0].value) {
+      player.hand.push(...prize)
+      console.log(`${player.name} wins ${prize.length} cards`)
+    } else if (playerWarCards[0].value < computerWarCards[0].value) {
+      computer.hand.push(...prize)
+      console.log(`${computer.name} wins ${prize.length} cards`)
+    } else {
+      declareWar()
+    }
+  }
+}
+
+playWar(player, computer)
